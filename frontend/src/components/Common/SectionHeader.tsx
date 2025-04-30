989f7698cc717b0d5279b049bc56bf5c44c0f798
olivershooter <olivershooter@gmail.com>
2025-04-30 22:15:28 +0100

    section header component

---
+++ frontend/src/components/Common/SectionHeader.tsx
@@ -0,0 +1,5 @@
+export const SectionHeader = ({ title }: { title: string }) => (
+  <h2 className="mb-8 rounded bg-gradient-to-r from-emerald-50 via-teal-100 to-teal-200 px-2 py-6 text-center text-lg font-bold sm:text-base md:text-2xl">
+    {title}
+  </h2>
+);
