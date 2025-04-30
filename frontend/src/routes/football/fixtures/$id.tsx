731ed189a61da82c6c9a7e0b830a7d67fb52a1eb
olivershooter <olivershooter@gmail.com>
2025-04-30 22:15:21 +0100

    refactoring and cleaning up the code

--- frontend/src/routes/football/fixtures/$id.tsx
+++ frontend/src/routes/football/fixtures/$id.tsx
@@ -1,9 +1,10 @@
+import { SectionHeader } from "@/components/Common/SectionHeader";
 import { ShirtSVG } from "@/components/FootballFixture/ShirtSVG";
 import { useGetRequest } from "@/hooks/useGetRequest";
 import { createFileRoute } from "@tanstack/react-router";
 import { AxiosHeaders } from "axios";
 
-const options = {
+const API_OPTIONS = {
   url: "/api/football/fixtures?id=",
   headers: {
     "x-rapidapi-key": import.meta.env.VITE_FOOTBALL_API_KEY,
@@ -12,223 +13,202 @@ const options = {
 };
 
 export const Route = createFileRoute("/football/fixtures/$id")({
-  component: footballFixture
+  component: FootballFixture
 });
 
-function footballFixture() {
+function FootballFixture() {
   const { id } = Route.useParams();
-  const axiosHeaders = new AxiosHeaders(options.headers);
+  const axiosHeaders = new AxiosHeaders(API_OPTIONS.headers);
 
   const { data, error, isPending } = useGetRequest({
-    url: options.url + id,
+    url: `${API_OPTIONS.url}${id}`,
     queryKey: "footballFixture",
     header: axiosHeaders,
-    gcTime: 1000 * 60 * 24
+    gcTime: 1000 * 60 * 60 * 24 * 7 // 1 week caching
   });
 
   if (error)
     return <div className="alert">Error loading fixture: {error.message}</div>;
-
   if (isPending) return <div className="loading">Loading...</div>;
 
   const fixtureData = data.response[0];
   const { teams, league, score, fixture, events, lineups, statistics } =
     fixtureData;
-
-  console.log("fixture data: ", fixtureData);
-
   const newDate = new Date(fixture.date).toLocaleString();
 
-  const positionOfPlayer = (position: string) => {
-    const positions: Record<string, string> = {
-      D: "Defender",
-      G: "Goalkeeper",
-      M: "Midfielder",
-      F: "Forward"
-    };
-    return positions[position] || "Unknown position";
+  const positionMap: Record<string, string> = {
+    D: "Defender",
+    G: "Goalkeeper",
+    M: "Midfielder",
+    F: "Forward"
   };
 
-  const whoWon = (homeTeamScore: string, awayTeamScore: string) => {
-    const homeIsWinner = homeTeamScore > awayTeamScore;
-    const tie = homeTeamScore === awayTeamScore;
+  const getScoreColor = (
+    isHome: boolean,
+    isWinner: boolean,
+    isTie: boolean
+  ) => {
+    if (isTie) return "text-slate-700";
+    return isWinner === isHome ? "text-lime-700" : "text-red-700";
+  };
+
+  const renderScoreline = (homeScore: number, awayScore: number) => {
+    const isTie = homeScore === awayScore;
+    const isHomeWinner = homeScore > awayScore;
 
     return (
-      <>
-        <div className="flex w-full flex-row items-center gap-2 text-sm font-bold sm:text-base md:text-2xl">
-          <div className="flex w-full items-center justify-end">
-            <div className="text-right">{teams.home.name}</div>
-            <div
-              className={`ml-2 ${
-                homeIsWinner
-                  ? "text-lime-700"
-                  : tie
-                    ? "text-slate-700"
-                    : "text-red-700"
-              }`}
-            >
-              {score.fulltime.home}
-            </div>
-          </div>
-          <div className="mx-2 flex items-center">-</div>
-          <div className="flex w-full items-center">
-            <div
-              className={`mr-2 ${
-                homeIsWinner
-                  ? "text-red-700"
-                  : tie
-                    ? "text-slate-700"
-                    : "text-lime-700"
-              }`}
-            >
-              {score.fulltime.away}
-            </div>
-            <div>{teams.away.name}</div>
+      <div className="flex w-full flex-row items-center justify-center gap-2 text-sm font-bold sm:text-base md:text-2xl">
+        <div className="flex w-full items-center justify-end">
+          <div className="text-right">{teams.home.name}</div>
+          <div className={`ml-2 ${getScoreColor(true, isHomeWinner, isTie)}`}>
+            {homeScore}
           </div>
         </div>
-      </>
+        <div className="mx-2">-</div>
+        <div className="flex w-full items-center">
+          <div className={`mr-2 ${getScoreColor(false, isHomeWinner, isTie)}`}>
+            {awayScore}
+          </div>
+          <div>{teams.away.name}</div>
+        </div>
+      </div>
+    );
+  };
+
+  const renderLineup = (teamIndex: number, alignment: "left" | "right") => {
+    const team = lineups[teamIndex];
+    return (
+      <div className="space-y-4">
+        <h3
+          className={`text-sm font-semibold sm:text-base md:text-xl text-${alignment}`}
+        >
+          {team.team.name} Starting XI
+        </h3>
+        {team.startXI.map(({ player }: { player: any }) => (
+          <div
+            key={player.id}
+            className="flex items-center justify-between rounded bg-gray-300 p-2"
+          >
+            <div className="flex items-center gap-2">
+              <ShirtSVG
+                width={40}
+                height={40}
+                playersNumber={player.number.toString()}
+                colour={`#${team.team.colors.player.primary}`}
+                alt={player.name}
+              />
+              {player.name}
+            </div>
+            <span>{positionMap[player.pos] || "Unknown position"}</span>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  const renderStatistics = (teamIndex: number, teamName: string) => {
+    const stats = statistics[teamIndex].statistics;
+    const getStat = (type: string) =>
+      stats.find((s: any) => s.type === type)?.value;
+
+    return (
+      <div className="space-y-4">
+        <h3 className="text-lg font-semibold">{teamName} Statistics</h3>
+        <div className="space-y-2">
+          {[
+            "Ball Possession",
+            "Shots on Goal",
+            "Corner Kicks",
+            "Yellow Cards"
+          ].map((stat) => (
+            <div key={stat} className="flex justify-between">
+              <span>{stat}:</span>
+              <span>{getStat(stat)}</span>
+            </div>
+          ))}
+        </div>
+      </div>
     );
   };
 
   return (
     <>
-      {/* Main Event Details */}
-      <div className="mb-8 grid grid-cols-1 gap-4 md:grid-cols-3">
-        {/* Team Info */}
-        <div className="col-span-1">
-          <div className="mb-8 flex flex-col gap-4">
-            <h2 className="text-lg font-bold sm:text-base md:text-2xl">
-              Match Details
-            </h2>
-            <div className="text-sm sm:text-base md:text-lg">
-              <p>
-                <b>Status: </b>
-                <span className="text-gray-600">{fixture.status.long}</span>
-              </p>
-              <p>
-                <b>League: </b>
-                <span className="text-gray-600">{league.name}</span>
-              </p>
-              <p>
-                <b>Season: </b>
-                <span className="text-gray-600">{league.season}</span>
-              </p>
-              <p>
-                <b>Round: </b>
-                <span className="text-gray-600">{league.round}</span>
-              </p>
-            </div>
+      {/* Scoreline */}
+      <div className="mb-8">
+        <SectionHeader title="Scoreline" />
+        <div className="flex flex-col items-center gap-2">
+          {renderScoreline(score.fulltime.home, score.fulltime.away)}
+          <div className="text-xs text-gray-600 sm:text-sm">
+            HT: {score.halftime.home} - {score.halftime.away}
+          </div>
+        </div>
+      </div>
+
+      {/* Match Details */}
+      <div className="mb-8 grid grid-cols-1 gap-4 md:grid-cols-2">
+        <div>
+          <h2 className="mb-4 text-lg font-bold sm:text-base md:text-2xl">
+            Match Details
+          </h2>
+          <div className="text-sm sm:text-base md:text-lg">
+            <p>
+              <b>Status:</b>{" "}
+              <span className="text-gray-600">{fixture.status.long}</span>
+            </p>
+            <p>
+              <b>League:</b>{" "}
+              <span className="text-gray-600">{league.name}</span>
+            </p>
+            <p>
+              <b>Season:</b>{" "}
+              <span className="text-gray-600">{league.season}</span>
+            </p>
+            <p>
+              <b>Round:</b>{" "}
+              <span className="text-gray-600">{league.round}</span>
+            </p>
           </div>
         </div>
 
-        {/* Scoreline */}
-        <div className="col-span-1">
-          <div className="mb-8 flex flex-col gap-4">
-            <h2 className="text-center text-lg font-bold sm:text-base md:text-2xl">
-              Scoreline
-            </h2>
-            <div className="flex flex-col items-center gap-2">
-              {whoWon(score.fulltime.home, score.fulltime.away)}
-              <div className="items-center gap-2 text-xs text-gray-600 sm:text-sm">
-                HT: {score.halftime.home} - {score.halftime.away}
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Match Header */}
-        <div className="col-span-1">
-          <div className="mb-8 flex flex-col gap-4">
-            <div className="text-right text-sm sm:text-base md:text-lg">
-              <p>
-                <b>Date: </b> <span className="text-gray-600">{newDate}</span>
-              </p>
-              <p>
-                <b>Venue: </b>
-                <span className="text-gray-600">{fixture.venue.name}</span>
-              </p>
-              <p>
-                <b>Referee: </b>
-                <span className="text-gray-600">{fixture.referee}</span>
-              </p>
-            </div>
+        <div>
+          <h2 className="mb-4 text-lg font-bold sm:text-base md:text-2xl">
+            Match Header
+          </h2>
+          <div className="text-sm sm:text-base md:text-lg">
+            <p>
+              <b>Date:</b> <span className="text-gray-600">{newDate}</span>
+            </p>
+            <p>
+              <b>Venue:</b>{" "}
+              <span className="text-gray-600">{fixture.venue.name}</span>
+            </p>
+            <p>
+              <b>Referee:</b>{" "}
+              <span className="text-gray-600">{fixture.referee}</span>
+            </p>
           </div>
         </div>
       </div>
 
       {/* Lineups */}
-      <div className="mb-8">
-        <h2 className="mb-4 text-xl font-bold">Lineups</h2>
+      <div className="mb-12">
+        <SectionHeader title="Lineups" />
         <div className="grid grid-cols-2 gap-6">
-          {/* Home Team Lineup */}
-          <div className="space-y-4">
-            <h3 className="text-lg font-semibold">
-              {teams.home.name} Starting XI
-            </h3>
-            {lineups[0].startXI.map((playerObj: { player: any }) => {
-              const player = playerObj.player;
-              return (
-                <div
-                  key={player.id}
-                  className="flex items-center justify-between rounded bg-gray-300 p-2"
-                >
-                  <div className="flex items-center gap-2">
-                    <span className="font-medium">
-                      <ShirtSVG
-                        width={40}
-                        height={40}
-                        playersNumber={player.number.toString()}
-                        colour={`#${lineups[0].team.colors.player.primary}`}
-                        alt={player.name}
-                      />
-                    </span>
-                    {player.name}
-                  </div>
-                  <span>{positionOfPlayer(player.pos)}</span>
-                </div>
-              );
-            })}
-          </div>
-
-          {/* Away Team Lineup */}
-          <div className="space-y-4">
-            <h3 className="text-lg font-semibold">
-              {teams.away.name} Starting XI
-            </h3>
-            {lineups[1].startXI.map((playerObj: { player: any }) => {
-              const player = playerObj.player;
-              return (
-                <div
-                  key={player.id}
-                  className="flex items-center justify-between rounded bg-gray-300 p-2"
-                >
-                  <div className="flex items-center gap-2">
-                    <span className="font-medium">
-                      <ShirtSVG
-                        width={40}
-                        height={40}
-                        playersNumber={player.number.toString()}
-                        colour={`#${lineups[1].team.colors.player.primary}`}
-                        alt={player.name}
-                      />
-                    </span>
-                    {player.name}
-                  </div>
-                  <span>{positionOfPlayer(player.pos)}</span>
-                </div>
-              );
-            })}
-          </div>
+          {renderLineup(0, "left")}
+          {renderLineup(1, "right")}
         </div>
       </div>
 
       {/* Events Timeline */}
-      <div className="mb-8">
-        <h2 className="mb-4 text-xl font-bold">Event Timeline</h2>
+      <div className="mb-8 mt-12">
+        <SectionHeader title="Event Timeline" />
         <div className="overflow-x-auto">
           <div className="flex space-x-4">
             {events.map((event: any) => (
-              <div className="flex flex-col items-center rounded bg-gray-100 p-2">
+              <div
+                key={event.time.elapsed + event.player.id}
+                className="flex flex-col items-center rounded bg-gray-100 p-2"
+              >
                 <span className="text-xs text-gray-500">
                   {event.time.elapsed}'
                 </span>
@@ -243,101 +223,11 @@ function footballFixture() {
       </div>
 
       {/* Statistics */}
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
-        <div className="space-y-4">
-          <h3 className="text-lg font-semibold">
-            {teams.home.name} Statistics
-          </h3>
-          <div className="space-y-2">
-            <div className="flex justify-between">
-              <span>Possession:</span>
-              <span>
-                {
-                  statistics[0].statistics.find(
-                    (stats: any) => stats.type === "Ball Possession"
-                  )?.value
-                }
-              </span>
-            </div>
-            <div className="flex justify-between">
-              <span>Shots On Goal:</span>
-              <span>
-                {
-                  statistics[0].statistics.find(
-                    (stats: any) => stats.type === "Shots on Goal"
-                  )?.value
-                }
-              </span>
-            </div>
-            <div className="flex justify-between">
-              <span>Corners:</span>
-              <span>
-                {
-                  statistics[0].statistics.find(
-                    (stats: any) => stats.type === "Corner Kicks"
-                  )?.value
-                }
-              </span>
-            </div>
-            <div className="flex justify-between">
-              <span>Yellow Cards:</span>
-              <span>
-                {
-                  statistics[0].statistics.find(
-                    (stats: any) => stats.type === "Yellow Cards"
-                  )?.value
-                }
-              </span>
-            </div>
-          </div>
-        </div>
-
-        <div className="space-y-4">
-          <h3 className="text-lg font-semibold">
-            {teams.away.name} Statistics
-          </h3>
-          <div className="space-y-2">
-            <div className="flex justify-between">
-              <span>Possession:</span>
-              <span>
-                {
-                  statistics[1].statistics.find(
-                    (stats: any) => stats.type === "Ball Possession"
-                  )?.value
-                }
-              </span>
-            </div>
-            <div className="flex justify-between">
-              <span>Shots On Goal:</span>
-              <span>
-                {
-                  statistics[1].statistics.find(
-                    (stats: any) => stats.type === "Shots on Goal"
-                  )?.value
-                }
-              </span>
-            </div>
-            <div className="flex justify-between">
-              <span>Corners:</span>
-              <span>
-                {
-                  statistics[1].statistics.find(
-                    (stats: any) => stats.type === "Corner Kicks"
-                  )?.value
-                }
-              </span>
-            </div>
-            <div className="flex justify-between">
-              <span>Yellow Cards:</span>
-              <span>
-                {
-                  statistics[1].statistics.find(
-                    (stats: any) => stats.type === "Yellow Cards"
-                  )?.value
-                }
-              </span>
-            </div>
-          </div>
+      <div className="mb-16">
+        <SectionHeader title="Statistics" />
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+          {renderStatistics(0, teams.home.name)}
+          {renderStatistics(1, teams.away.name)}
         </div>
       </div>
     </>
