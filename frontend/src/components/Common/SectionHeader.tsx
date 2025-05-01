13dc38e8ddf1c42ce66f81aff4afa6c87ea4f414
olivershooter <olivershooter@gmail.com>
2025-05-01 23:33:19 +0100

    fixing the section header

--- frontend/src/components/Common/SectionHeader.tsx
+++ frontend/src/components/Common/SectionHeader.tsx
@@ -1,5 +1,14 @@
-export const SectionHeader = ({ title }: { title: string }) => (
-  <h2 className="mb-8 rounded bg-gradient-to-r from-emerald-50 via-teal-100 to-teal-200 px-2 py-6 text-center text-lg font-bold sm:text-base md:text-2xl">
-    {title}
+import { HorizontalLine } from "./HorizontalLine";
+
+export const SectionHeader = ({
+  title,
+  svgName
+}: {
+  title: string;
+  svgName?: string;
+}) => (
+  <h2 className="mb-1 px-2 py-6 text-center text-lg font-bold sm:text-base md:text-2xl">
+    {title.toUpperCase()}
+    {svgName ? <HorizontalLine svgName={svgName} /> : <HorizontalLine />}
   </h2>
 );
