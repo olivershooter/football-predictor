e958d08533e6ddad14f0b642a88c1852fa581906
olivershooter <olivershooter@gmail.com>
2025-05-10 23:55:07 +0100

    trying to fix cors issue on vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,11 @@ import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
+import vercel from "vite-plugin-vercel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite(), vercel()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
@@ -16,6 +17,7 @@ export default defineConfig({
       "/api/football": {
         target: "https://v3.football.api-sports.io",
         changeOrigin: true,
+        secure: false,
         rewrite: (path) => path.replace(/^\/api\/football/, "")
       }
     }
